d0439b1367d64403b9b08c4733a5b4d826e0ca9b
Michal <malek.michal.j@gmail.com>
2022-03-29 19:16:14 -0500

    UI Update
    Implemented slide gesture and bottom nav bar and made UI a bit cleaner

--- src/frontend/IPRO497-Group-I/app/src/main/java/com/example/ipro497_group_i/MainActivity.java
+++ src/frontend/IPRO497-Group-I/app/src/main/java/com/example/ipro497_group_i/MainActivity.java
@@ -1,57 +1,173 @@
 package com.example.ipro497_group_i;
 
-import static android.content.ContentValues.TAG;
-
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.view.MenuItem;
 import android.view.Menu;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
+import com.example.ipro497_group_i.ui.OnSwipeTouchListener;
+import com.example.ipro497_group_i.ui.checkinout.CheckInOutFragment;
+import com.example.ipro497_group_i.ui.gallery.GalleryFragment;
+import com.example.ipro497_group_i.ui.home.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import com.example.ipro497_group_i.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    int slide;
+
     FirebaseDatabase db;
     EditText text_home, age, time;
+    BottomNavigationView bottomNavigationView;
+    CheckInOutFragment qrFrag = new CheckInOutFragment();
+    HomeFragment homeFrag = new HomeFragment();
+    GalleryFragment thirdFragment = new GalleryFragment();
 
     private static final int PERMISSION_CAMERA = 0;
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_bar);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        slide = 2;
+
+        FrameLayout relativeLayout = (FrameLayout) findViewById(R.id.flFragment);
+        relativeLayout.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            /*public void onSwipeBottom() {
+                Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
+
+            }*/
+            public void onSwipeRight() {
+                //Toast.makeText(MainActivity.this, "right", Toast.LENGTH_SHORT).show();
+                if(slide == 2){
+                    //getSupportFragmentManager().beginTransaction().setCustomAnimations( R.anim.slide_out_left, R.anim.slide_out_right).replace(R.id.container, qrFrag).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, qrFrag).commit();
+                    bottomNavigationView.setSelectedItemId(R.id.scan);
+                    slide = 1;
+                }
+                if(slide == 1){
+
+                }
+                if(slide == 3){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFrag).commit();
+                    bottomNavigationView.setSelectedItemId(R.id.home);
+                    slide = 2;
+                }
+            }
+            public void onSwipeLeft() {
+                //Toast.makeText(MainActivity.this, "left", Toast.LENGTH_SHORT).show();
+                if(slide == 2) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
+                    bottomNavigationView.setSelectedItemId(R.id.reserve);
+                    slide = 3;
+                }
+                if(slide == 3){
+
+                }
+                if(slide == 1){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFrag).commit();
+                    bottomNavigationView.setSelectedItemId(R.id.home);
+                    slide = 2;
+                }
+            }
+
+        });
+        /*
+        Data data = new Data();
+        db = FirebaseDatabase.getInstance();
+        DatabaseReference bruh = db.getReference("Data");
+
+        age = findViewById(R.id.age);
+        time = findViewById(R.id.date);
+
+        @Override
+        public void onClick(View view) {
+
+            int old = Integer.parseInt(age.getText().toString().trim());
+            int peepNum = Integer.parseInt(time.getText().toString().trim());
+
+            //data.setName(text_home.getText());
+            data.setAge(old);
+            data.setNum(peepNum);
+
+            bruh.setValue(data);
+            Snackbar.make(view, "Data Sent To DataBase", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+                bruh.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // This method is called once with the initial value and again
+                        // whenever data at this location is updated.
+                        String value = dataSnapshot.getValue(String.class);
+                        Log.d(TAG, "Value is: " + value);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+                        // Failed to read value
+                        Log.w(TAG, "Failed to read value.", error.toException());
+                    }*/
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.scan:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, qrFrag).commit();
+                return true;
+
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFrag).commit();
+                return true;
+
+            case R.id.reserve:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, thirdFragment).commit();
+                return true;
+        }
+        return false;
+    }
+}
+
+
+        /*
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+
+
+        });
+        */
+
+
+
+
+
+        /*
 
         setSupportActionBar(binding.appBarMain.toolbar);
 
@@ -66,61 +182,15 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        */
 
 
-        Data data = new Data();
-        db = FirebaseDatabase.getInstance();
-        DatabaseReference bruh = db.getReference("Data");
 
-        age = findViewById(R.id.age);
-        time = findViewById(R.id.date);
-
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                
-                int old = Integer.parseInt(age.getText().toString().trim());
-                int peepNum = Integer.parseInt(time.getText().toString().trim());
-
-                //data.setName(text_home.getText());
-                data.setAge(old);
-                data.setNum(peepNum);
-                
-                bruh.setValue(data);
-                Snackbar.make(view, "Data Sent To DataBase", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-                /*bruh.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        // This method is called once with the initial value and again
-                        // whenever data at this location is updated.
-                        String value = dataSnapshot.getValue(String.class);
-                        Log.d(TAG, "Value is: " + value);
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        // Failed to read value
-                        Log.w(TAG, "Failed to read value.", error.toException());
-                    }
-                });*/
-            }
-        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
+    /*
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-}
+    */
